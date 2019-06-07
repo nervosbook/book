6e08a0a99332643029ed6b2c01b80431d95d21e1
Billie Zhang <billiecoder@gmail.com>
2019-06-07 22:36:49 +0800

    refactor code generating pdf

--- tools/gen-pdf.js
+++ tools/gen-pdf.js
@@ -8,6 +8,29 @@ const ejs = require('ejs')
 const bookDir = resolve(__dirname, `../content/book`)
 const tempDir = resolve(__dirname, `./template`)
 
+const sectionTemp = `
+  <section class="<%- className %>" id="<%- id %>">
+    <h1>
+      <%- title %>
+    </h1>
+    <%- html %>
+  </section>
+`
+
+const liTemp = `
+  <li class="<%- className %>">
+    <a href="#<%- id %>">
+      <%- title %>
+    </a>
+    <% if (!!html) { %>
+    <ul>
+      <%- html %>
+    </ul>
+    <% } %>
+  </li>
+`
+
+// There are two versions of the ebook
 function getBooks() {
   const file = readFileSync(`${bookDir}/../index.yml`, 'utf8')
   return safeLoad(file)
@@ -22,13 +45,18 @@ function getChapterContent(sections, bookId) {
   return sections
     .map(section => {
       const path = `${bookDir}/${bookId}/${section.id}.md`
-
       // convert markdown to html
       const html = execSync(`pandoc -f markdown -t html ${path}`).toString()
-
-      return `<section class="section" id="${section.id}">\n<h1>${
-        section.title
-      }</h1>\n${html}</section>`
+      try {
+        return ejs.render(sectionTemp, {
+          className: 'section',
+          id: section.id,
+          title: section.title,
+          html
+        })
+      } catch (err) {
+        console.log('ejs render section err...', err)
+      }
     })
     .join('\n')
 }
@@ -36,41 +64,52 @@ function getChapterContent(sections, bookId) {
 function getTocContent(toc) {
   return toc
     .map(el => {
-      const list = el.items
-        .map(item => {
-          return `<li class="section">\n<a href="#${item.id}">${
-            item.title
-          }</a>\n</li>`
-        })
+      const html = el.items
+        .map(item =>
+          ejs.render(liTemp, {
+            className: 'section',
+            id: item.id,
+            title: item.title,
+            html: ''
+          })
+        )
         .join('\n')
-
-      return `<li class="chapter">\n<a href="#${el.title}">${
-        el.title
-      }</a>\n<ul>${list}</ul>\n</li>`
+      return ejs.render(liTemp, {
+        className: 'chapter',
+        id: el.title,
+        title: el.title,
+        html
+      })
     })
     .join('\n')
 }
 
+// Remove old pdf directory, then create a new one in the directory executing this script
 execSync(`rm -rf pdf && mkdir pdf`)
 
 getBooks().forEach(book => {
   const toc = readBookToc(book.id)
   const content = toc
     .map(el => {
-      const chapter = getChapterContent(el.items, book.id)
-      return `<section class="chapter" id="${el.title}">\n<h1>${
-        el.title
-      }</h1>\n${chapter}\n</section>`
+      const html = getChapterContent(el.items, book.id)
+      try {
+        const result = ejs.render(sectionTemp, {
+          className: 'chapter',
+          id: el.title,
+          title: el.title,
+          html
+        })
+        return result
+      } catch (err) {
+        console.log('ejs render chapter err...', err)
+      }
     })
     .join('\n')
 
-  const tocContent = getTocContent(toc)
-  const tempFile = readFileSync(`${tempDir}/${book.id}.html`, 'utf8')
-  const bookHtml = ejs.render(`${tempFile}`, { content, toc: tocContent })
-
+  const bookTemp = readFileSync(`${tempDir}/${book.id}.html`, 'utf8')
+  const bookHtml = ejs.render(bookTemp, { content, toc: getTocContent(toc) })
   const bookHtmlPath = `${tempDir}/book-${book.id}.html`
   writeFileSync(bookHtmlPath, bookHtml)
-
   // convert html file to pdf ebook
   execSync(
     `prince ${bookHtmlPath} && mv ${tempDir}/*.pdf pdf && rm ${bookHtmlPath} `
